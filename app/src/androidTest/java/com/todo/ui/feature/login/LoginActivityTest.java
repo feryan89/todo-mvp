f7e16f2c82a7d196f1e6696fe718de75b5f9932f
waleedsarwar <waleed_sarwar@hotmail.com>
2018-04-27 01:39:59 +0500

    Add Jacoco code coverage plugin Update circle ci config.

--- app/src/androidTest/java/com/todo/ui/feature/login/LoginActivityTest.java
+++ app/src/androidTest/java/com/todo/ui/feature/login/LoginActivityTest.java
@@ -1,12 +1,17 @@
 package com.todo.ui.feature.login;
 
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
+@RunWith(AndroidJUnit4.class)
 public class LoginActivityTest {
 
-    @Before
-    public void setUp() throws Exception {
-    }
+    @Rule
+    public ActivityTestRule<LoginActivity> loginActivityActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
 }
--- app/src/androidTest/java/com/todo/ui/feature/register/RegisterActivityTest.java
+++ app/src/androidTest/java/com/todo/ui/feature/register/RegisterActivityTest.java
@@ -1,7 +1,14 @@
 package com.todo.ui.feature.register;
 
-import static org.junit.Assert.*;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
 public class RegisterActivityTest {
 
+    @Rule
+    public ActivityTestRule<RegisterActivity> registerActivityActivityTestRule = new ActivityTestRule<>(RegisterActivity.class);
 }
